f52db1929120fe1f52501a9b016117fc2773d91c
Daniel Patino <Daniel Patino>
2017-03-20 06:05:57 -0400

    Magical_Word -In Prog

--- Magical_Word.cpp
+++ Magical_Word.cpp
@@ -1,11 +1,4 @@
 /*
- *  Dhananjay has recently learned about ASCII values.
- *  He is very fond of experimenting. With his knowledge of ASCII values and character he has developed a special word and named it Dhananjay's Magical word.
- *  A word which consist of alphabets whose ASCII values is a prime number is an Dhananjay's Magical word. An alphabet is Dhananjay's Magical alphabet if its ASCII value is prime.
- *  Dhananjay's nature is to boast about the things he know or have learnt about.
- *  So just to defame his friends he gives few string to his friends and ask them to convert it to Dhananjay's Magical word.
- *  None of his friends would like to get insulted. Help them to convert the given strings to Dhananjay's Magical Word.
- *  
  *  First line of input contains an integer T number of test cases. Each test case contains an integer N (denoting the length of the string) and a string S.
  */
 
